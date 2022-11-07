9977b52fe112e8d87d8cac6ac8e7ea1a3239a8b4
jhon sebastian zuñiga <jhonsebastian2250@gmail.com>
2022-11-07 11:00:59 -0500

    conexion a la base de datos

---
+++ model/connection_db.py
@@ -0,0 +1,19 @@
+from aplication import app
+from flask_mysqldb import MySQL
+from MySQLdb.cursors import Cursor
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'movie_flask'
+app.config['MYSQL_PORT'] = 3606
+
+mysql = MySQL(app)
+
+def execute(sql:str) -> Cursor:
+    cursor = mysql.connection.cursor()
+    cursor.execute(sql)
+    return cursor
+
+def commit() -> None:
+    mysql.connection.comment()
